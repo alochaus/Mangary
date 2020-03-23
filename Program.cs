5341c900ddb54b14f5e180c62617548c0f4d534c
alochaus <aloc@techie.com>
2020-03-22 22:33:23 -0400

    refactor: I guess I fixed my mess

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Mangary.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Mangary
 {
